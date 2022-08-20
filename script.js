a9556352013f86b5a1caecf22ecdb891487954d7
Summaiya Haider <summaiya@student.iul.ac.in>
2022-08-21 01:22:06 +0530

    Completed

--- script.js
+++ script.js
@@ -16,4 +16,48 @@ function getLowercase(){
 function getUppercase(){
     return upperLetters[Math.floor(Math.random() * upperLetters.length)];
 }
+function getSymbol(){
+    return symbol[Math.floor(Math.random() * symbol.length)];
+}
+function generatePassowrd(){
+    const len = lenEl.value;
+    let password = "";
+    for(let i = 0; i < len; i++){
+        const x = generateX();
+        password += x;
+    }
+    PwEl.innerText = password;
+    function generateX(){
+        const xs = [];
+        if(upperEl.checked){
+            xs.push(getUppercase());
+        }
+        if(upperEl.checked){
+            xs.push(getLowercase());
+        }
+        if(upperEl.checked){
+            xs.push(getNumber());
+        }
+        if(upperEl.checked){
+            xs.push(getSymbol());
+        }
+        if (xs.length === 0) return "";
+        return xs[Math.floor(Math.random() * xs.length)];
+}
+        generateEl.addEventListener("click", generatePassowrd);
+        copyEl.addEventListener("click",() => {
+            const textarea = document.createElement("textarea");
+            const password = PwEl.innerText;
+            if(!password){
+                return;
+            }
+            textarea.value = password;
+            document.body.appendChild(textarea);
+            textarea.select();
+            document.execCommand("copy");
+            textarea.remove();
+            alert("password copied to clipboard");
+        });
+}
+console.log("hello");
 
